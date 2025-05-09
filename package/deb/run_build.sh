6e6c789bc2a8e20690ac4e20a2939f8183fae752
Vadim Kalinnikov <moose@ylsoftware.com>
2025-05-09 22:53:15 +0300

    Some refactorig in packaging scripts

--- package/deb/run_build.sh
+++ package/deb/run_build.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-cd $(dirname $0)
+FULLPATH=$(readlink -f "$0")
+
+BASEPATH=$(dirname "$FULLPATH")
+
+cd "$BASEPATH" || exit
 
 # Point to root dir of source code
 ROOTPATH=$(readlink -f "./../../")
@@ -12,7 +16,7 @@ PKG_VER="$1"
 PPA_VER="$2"
 
 
-if [ -z $PPA_VER ]; then
+if [ -z "$PPA_VER" ]; then
 	echo "Usage:"
 	echo "  $0 VERSION BUILD"
 	echo " "
@@ -22,20 +26,20 @@ if [ -z $PPA_VER ]; then
 fi
 
 # Cleanup
-rm -rf $OUTPUTDIR
-mkdir -p $OUTPUTDIR
+rm -rf "$OUTPUTDIR"
+mkdir -p "$OUTPUTDIR"
 
-mkdir -p $ROOTPATH/.cache/apt
+mkdir -p "$ROOTPATH/.cache/apt"
 
-for dockerfile in `ls dockerfiles/*.Dockerfile`; do
-	IMGNAME=$(echo $(basename $dockerfile) | sed 's/.Dockerfile//')
-	echo $IMGNAME
+for dockerfile in dockerfiles/*.Dockerfile; do
+	IMGNAME=$(basename ${dockerfile/.Dockerfile//})
+	echo "$IMGNAME"
 
-	docker build -f $dockerfile -t $IMGNAME $ROOTPATH
+	docker build -f "$dockerfile" -t "$IMGNAME" "$ROOTPATH"
 	docker run --rm -it \
-		-e PKG_VERSION=${PKG_VER} \
-		-e PKG_PPAVER=${PPA_VER} \
-		-v $OUTPUTDIR:/release \
-		-v $ROOTPATH:/src \
-		-t $IMGNAME
+		-e PKG_VERSION="${PKG_VER}" \
+		-e PKG_PPAVER="${PPA_VER}" \
+		-v "$OUTPUTDIR:/release" \
+		-v "$ROOTPATH:/src" \
+		-t "$IMGNAME"
 done
--- package/deb/run_ppa_upload.sh
+++ package/deb/run_ppa_upload.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-cd $(dirname $0)
+FULLPATH=$(readlink -f "$0")
 
-for sc_file in `ls ../../release/*.changes`; do
+BASEPATH=$(dirname "$FULLPATH")
+
+cd "$BASEPATH" || exit
+
+for sc_file in  ../../release/*.changes; do
 	echo "Processing ${sc_file}:"
-	dput ppa:bulvinkl/ppa $sc_file
+	dput ppa:bulvinkl/ppa "${sc_file}"
 	echo ""
 	echo ""
 done
